1bb78c254161cda366c3fff22d5f6a2003ba2a31
Geon Peter <igeonpeter@gmail.com>
2024-09-09 15:08:52 +0200

    Initial Commit

---
+++ job.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import timedelta,datetime
+
+def task1_function():
+    print('Task1 completed.')
+
+def task2_function():
+    print('Task2 completed.')
+
+default_args = {
+    'owner' : 'air0',
+    'start_date' : datetime(2029,09,09),
+    'retries' : 1,
+    'retry_delay' : timedelta(minutes=2)
+}
+
+with DAG(
+    'air_dag',
+    default_args = default_args,
+    schedule_interval = timedelta(minutes=2)
+) as dag:
+    
+    task1 = PythonOperator(task_id = 'task1', python_callable= task1_function)
+
+    sleep = BashOperator(task_id = 'sleep', bash_command= 'sleep 2')
+
+    task2 = PythonOperator(task_id = 'task2', python_callable= task2_function)
+    
+  
+
+    task1 >> sleep >> task2
